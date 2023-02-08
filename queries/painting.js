36a0f6ec9c3901c3a9d3754185842674f7ca7718
Tech-Kat <katpelle@pursuit.org>
2023-02-08 16:25:15 -0500

    testing

--- queries/painting.js
+++ queries/painting.js
@@ -9,8 +9,22 @@ const getAllPaintings = async () => {
     }
 };
 
+const getPainting = async (id) => {
+    try{
+        const onePainting = await db.oneOrNone("SELECT * FROM paintings WHERE id=$1", id);
+        return onePainting;
+    }catch (error) {
+        return error;
+    }
+};
 
+const createPainting = async (painting) => {
+    const {name, artist, price, size, description, is_available, image}= painting;
+    try{
+        const newPainting = await db.oneOrNone("INSERT INTO paintings (name, INITCAP(artist), price, size, description, is_available, image)")
+    }
+}
 
 module.exports = {
-    getAllPaintings
+    getAllPaintings, getPainting,
 }
